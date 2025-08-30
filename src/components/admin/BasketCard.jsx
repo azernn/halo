46e6b8eeb68c86bef291790909ab57af857e6977
azernn <127416294+azernn@users.noreply.github.com>
2025-08-30 18:20:05 +0400

    basket added

--- src/components/admin/BasketCard.jsx
+++ src/components/admin/BasketCard.jsx
@@ -1,42 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CartContext } from '../context/BasketContext'
 
 function BasketCard() {
+    const { cart } = useContext(CartContext)
+    
     return (
-        <div className='bg-white w-[330px] text-black p-4  rounded-xl px-8 hidden md:block transform transition-all
+        
+        <div className='bg-white w-[330px] text-black p-4 rounded-xl px-8 hidden md:block transform transition-all
       duration-300 ease-in-out' >
-            <p className='text-center '> Card [3]</p>
+            <p className='text-center '> Card [{cart.length}]</p>
             <div className='overflow-y-auto max-h-[60vh]'>
                 <div className='mt-4 border-b border-gray-300 pb-3'  >
-                    <div className='flex  '>
+                   {
+                    cart.map(item=>(
+                         <div className='flex mt-4 '>
                         <div className=' flex flex-col w-[30%] gap-3'>
-                            <img className='w-[60%]' src="https://www.newlinehalo.dk/dw/image/v2/BDWL_PRD/on/demandware.static/-/Sites-halo-master-catalog/default/dwfd95fb48/images/packshot/234034-6477.png?sw=300&sh=300&q=80" alt="" />
-                            <p className='text-[11px] uppercase text-gray-400'> Qty : 1</p>
+                            <img className='w-[60%]' src={item.images[0]} alt="" />
+                            <p className='text-[11px] uppercase text-gray-400'> Qty : {item.quantity}</p>
                         </div>
                         <div className=''>
                             <div>
-                                <p className='text-[13px] uppercase' >HALO X MESSY WEEKEND</p>
-                                <p className='text-[13px]'>699,95DKK</p>
+                                <p className='text-[13px] uppercase' >{item.name}</p>
+                                <p className='text-[13px]'>$ {item.price}</p>
                             </div>
-                            <p className=' text-[11px]  my-2 text-gray-400'>Size:  ONE</p>
+                            <p className=' text-[11px]  my-2 text-gray-400'>Size: {item.size}</p>
                             <button className='text-[12px] font-semibold hover:underline'>Remove</button>
                         </div>
                     </div>
-                </div>
-                <div className='mt-4 border-b border-gray-300 pb-3'  >
-                    <div className='flex  '>
-                        <div className=' flex flex-col w-[30%] gap-3'>
-                            <img className='w-[60%]' src="https://www.newlinehalo.dk/dw/image/v2/BDWL_PRD/on/demandware.static/-/Sites-halo-master-catalog/default/dwfd95fb48/images/packshot/234034-6477.png?sw=300&sh=300&q=80" alt="" />
-                            <p className='text-[11px] uppercase text-gray-400'> Qty : 1</p>
-                        </div>
-                        <div className=''>
-                            <div>
-                                <p className='text-[13px] uppercase' >HALO X MESSY WEEKEND</p>
-                                <p className='text-[13px]'>699,95DKK</p>
-                            </div>
-                            <p className=' text-[11px]  my-2 text-gray-400'>Size:  ONE</p>
-                            <button className='text-[12px] font-semibold hover:underline'>Remove</button>
-                        </div>
-                    </div>
+                    ))
+                   }
                 </div>
             </div>
             <div className='flex justify-between font-bold uppercase text-[14px] my-4 '>
